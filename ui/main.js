4da31feac6f13227d3ee8823eb431476ff473f82
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-09-28 15:58:08 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ console.log('Loaded!');
 var img = document.getElementById('agent');
 var marginleft = 0;
 function moveRight() {
-  marginleft = marginleft + 10;
+  marginleft = marginleft + 1;
   imge.style.marginleft = marginleft + 'px';
 }
 img.onclick = function(){
-var interval = setInterval(moveRight,100);
+var interval = setInterval(moveRight,50);
 };
