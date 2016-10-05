b49012b4a2deda1c253a2e87f8b05585168e2466
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-10-05 18:15:53 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //move the image
 var img = document.getElementById('copy');
-var marginleft = 0;
+var marginLeft = 0;
 function moveRight() {
   marginLeft = marginLeft + 1;
   imge.style.marginLeft = marginLeft + 'px';
