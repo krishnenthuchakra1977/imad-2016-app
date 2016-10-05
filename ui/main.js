b19487fc97e0e1f5000562ab96286f2018d89235
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-10-05 22:42:47 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,30 @@ function moveRight() {
 img.onclick = function(){
 var interval = setInterval(moveRight,50);
 };
+//counter code 
+var button = document.getElementById('counter');
+button.onclick = function()
+{
+  // create a request object
+  var request = new XMLHttpRequest();
+  
+  // capture the responce and store it in a variable
+  request.onreadystatechange = function()
+  {
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+    //Take some action
+    if(request.status === 200)
+    {
+      var counter = request.responceText;
+      var span = document.getElementById('count');
+      span.innerHTML = counter.toString();
+    }
+  }
+    // Not done yet
+  };
+  // make the request
+  request.open('GET','http://krishnenthuchakra1977.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
+  
