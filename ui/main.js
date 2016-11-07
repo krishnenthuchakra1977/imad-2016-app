623b10638135ebb22b0386db84285256cc36c359
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-07 16:32:43 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,3 @@
-console.log('Loaded!');
-//move the image
-var img = document.getElementById('copy');
-var marginLeft = 0;
-function moveRight() {
-  marginLeft = marginLeft + 1;
-  img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-var interval = setInterval(moveRight,50);
-};
 //counter code 
 var button = document.getElementById('counter');
 button.onclick = function()
@@ -35,4 +24,37 @@ button.onclick = function()
   request.open('GET','http://krishnenthuchakra1977.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+  // submit name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
   
+  // create a request object
+  var request = new XMLHttpRequest();
+  
+  //capture the responce and storte it in a variable
+  request.onreadystatechange = function () {
+    if (request.readystate === XMLHttpRequest.DONE) {
+      // Take some action
+      if (request.status === 200) {
+        //capture a list of names and render it as a list
+        var names = request.responceText;
+        names = JSON.parse(names);
+        var list = '';
+        for (var i=0;i<names.length;i++) {
+          list += '<li>' + names[i] + '</li>';
+        }
+        var ul = document.getElementById('namelist');
+        ul.innerHTMl = list;
+      }
+    }
+    //not done yet
+  };
+  
+  //make the request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  request.open('GET','http://krishnenthuchakra1977.imad.hasura-app.io/submit-name?name=' + name,true);
+  request.send(null);
+  
+};
+     
