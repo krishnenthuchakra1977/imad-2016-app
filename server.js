48876455fd170da72c93311c3549a61e56ce464f
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-08 15:12:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-  user: 'krishnenthuchakra1977'
-  database: 'krishnenthuchakra1977'
+  user: 'krishnenthuchakra1977',
+  database: 'krishnenthuchakra1977',
   host: 'db.imad.hasura-app.io',
   port: '5432',
   password: process.env.DB_PASSWORD
