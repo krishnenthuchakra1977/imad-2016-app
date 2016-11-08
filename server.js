3711616779095fa48a8b9160c3d8f88293e45b60
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-08 11:16:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,9 +37,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/new-app.php', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'new-app.php'));
-})
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
