8ab6ef0602ae4817b0176280825a92e987fb76a5
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-09 12:07:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,13 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
+app.get('/ui/slide3.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'slide3.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
