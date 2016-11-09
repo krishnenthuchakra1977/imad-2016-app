071f4023b392655324325fb54555440ac9a3d451
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-09 15:51:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-;
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -20,9 +22,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/index.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
 
 app.get('/ui/slide3.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'slide3.png'));
