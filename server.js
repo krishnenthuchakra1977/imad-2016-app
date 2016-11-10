9495b2c5619378aae194f5fab895e942152613ef
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-10 22:46:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,202 +1,236 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+var config = {
+    user: 'coco98',
+    database: 'coco98',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
+                ${content}
+              </div>
+              <hr/>
+              <h4>Comments</h4>
+              <div id="comment_form">
+              </div>
+              <div id="comments">
+                <center>Loading comments...</center>
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/about.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
-app.get('/single.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'single.html'));
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
 });
 
-app.get('/trainers.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'trainers.html'));
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
 });
 
-app.get('/404.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '404.html'));
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 
-app.get('/blog.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
 
-app.get('/contact.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+var pool = new Pool(config);
+
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
 });
 
-app.get('/ui/copy.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'copy.jpg'));
+app.get('/get-comments/:articleName', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
 });
 
-app.get('/ui/slide3.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'slide3.png'));
+app.post('/submit-comment/:articleName', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
+            if (err) {
+                res.status(500).send(err.toString());
+            } else {
+                if (result.rows.length === 0) {
+                    res.status(400).send('Article not found');
+                } else {
+                    var articleId = result.rows[0].id;
+                    // Now insert the right comment for this article
+                    pool.query(
+                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        [req.body.comment, articleId, req.session.auth.userId],
+                        function (err, result) {
+                            if (err) {
+                                res.status(500).send(err.toString());
+                            } else {
+                                res.status(200).send('Comment inserted!')
+                            }
+                        });
+                }
+            }
+       });     
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
 });
 
-app.get('/ui/ab.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ab.jpg'));
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
 });
 
-app.get('/ui/arrow.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'arrow.png'));
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
-app.get('/ui/arrows.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'arrows.png'));
-});
-
-app.get('/ui/b1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'b1.jpg'));
-});
-
-app.get('/ui/b2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'b2.jpg'));
-});
-
-app.get('/ui/b3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'b3.jpg'));
-});
-
-app.get('/ui/banner-two.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'banner-two.jpg'));
-});
-
-app.get('/ui/banner.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'banner.jpg'));
-});
-
-app.get('/ui/blog1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'blog1.jpg'));
-});
-
-app.get('/ui/blog2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'blog2.jpg'));
-});
-
-app.get('/ui/blog3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'blog3.jpg'));
-});
-
-app.get('/ui/c1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'c1.jpg'));
-});
-
-app.get('/ui/c2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'c2.jpg'));
-});
-
-app.get('/ui/c3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'c3.jpg'));
-});
-
-app.get('/ui/co.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'co.png'));
-});
-
-app.get('/ui/img_spt.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'img_spt.png'));
-});
-
-app.get('/ui/menu-icon.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'menu-icon.jpg'));
-});
-
-app.get('/ui/move-top.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'move-top.png'));
-});
-
-app.get('/ui/n1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'j1.jpg'));
-});
-
-app.get('/ui/n2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'j2.jpg'));
-});
-
-app.get('/ui/n3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'j3.jpg'));
-});
-
-app.get('/ui/read.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'read.png'));
-});
-
-app.get('/ui/slide2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'slide2.jpg'));
-});
-
-app.get('/ui/slide3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'slide3.jpg'));
-});
-
-app.get('/ui/slide4.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'slide4.jpg'));
-});
-
-app.get('/ui/t1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 't1.jpg'));
-});
-
-app.get('/ui/t2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 't2.jpg'));
-});
-
-app.get('/ui/t3.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 't3.jpg'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/bootstrap.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/animate.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'animate.css'));
-});
-
-app.get('/ui/component.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'component.css'));
-});
-
-app.get('/ui/jquery.cbpFWSlider.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'jquery.cbpFWSlider.min.js'));
-});
-
-app.get('/ui/jquery.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'jquery.min.js'));
-});
-
-app.get('/ui/modernizr.custom.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'modernizr.custom.js'));
-});
-
-app.get('/ui/owl.carousel.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'owl.carousel.js'));
-});
-
-app.get('/ui/owl.carousel.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'owl.carousel.css'));
-});
-
-app.get('/ui/wow.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'wow.min.js'));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
