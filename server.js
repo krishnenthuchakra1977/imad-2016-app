ed3750e479fbd21bd8183f5607dc9c2dcb50aae8
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-12 01:14:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,22 +71,161 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'indexone.html'));
 });
 
-app.get('/ui/animate.css', function (req, res) {
+app.get('/ui/css/animate.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'animate.css'));
 });
-app.get('/ui/bootstrap.css', function (req, res) {
+app.get('/ui/css/bootstrap.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
 });
-app.get('/ui/component', function (req, res) {
+app.get('/ui/css/component', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'component.css'));
 });
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/css/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/owl.carousel.css', function (req, res) {
+app.get('/ui/css/owl.carousel.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'owl.carousel.css'));
 });
+app.get('/ui/images/slide3.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'slide3.png'));
+});
 
+app.get('/ui/images/ab.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ab.jpg'));
+});
+
+app.get('/ui/images/arrow.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'arrow.png'));
+});
+
+app.get('/ui/images/arrows.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'arrows.png'));
+});
+
+app.get('/ui/images/b1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'b1.jpg'));
+});
+
+app.get('/ui/images/b2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'b2.jpg'));
+});
+
+app.get('/ui/images/b3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'b3.jpg'));
+});
+
+app.get('/ui/images/banner-two.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'banner-two.jpg'));
+});
+
+app.get('/ui/images/banner.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'banner.jpg'));
+});
+
+app.get('/ui/images/blog1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blog1.jpg'));
+});
+
+app.get('/ui/images/blog2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blog2.jpg'));
+});
+
+app.get('/ui/images/blog3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blog3.jpg'));
+});
+
+app.get('/ui/images/c1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'c1.jpg'));
+});
+
+app.get('/ui/images/c2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'c2.jpg'));
+});
+
+app.get('/ui/images/c3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'c3.jpg'));
+});
+
+app.get('/ui/images/co.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'co.png'));
+});
+
+app.get('/ui/images/img_spt.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'img_spt.png'));
+});
+
+app.get('/ui/images/menu-icon.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'menu-icon.jpg'));
+});
+
+app.get('/ui/images/move-top.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'move-top.png'));
+});
+
+app.get('/ui/images/n1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'j1.jpg'));
+});
+
+app.get('/ui/images/n2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'j2.jpg'));
+});
+
+app.get('/ui/images/n3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'j3.jpg'));
+});
+
+app.get('/ui/images/read.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'read.png'));
+});
+
+app.get('/ui/images/slide2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'slide2.jpg'));
+});
+
+app.get('/ui/images/slide3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'slide3.jpg'));
+});
+
+app.get('/ui/images/slide4.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'slide4.jpg'));
+});
+
+app.get('/ui/images/t1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 't1.jpg'));
+});
+
+app.get('/ui/images/t2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 't2.jpg'));
+});
+
+app.get('/ui/images/t3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 't3.jpg'));
+});
+
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/js/jquery.cbpFWSlider.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jquery.cbpFWSlider.min.js'));
+});
+
+app.get('/ui/js/jquery.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jquery.min.js'));
+});
+
+app.get('/ui/js/modernizr.custom.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'modernizr.custom.js'));
+});
+
+app.get('/ui/js/owl.carousel.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'owl.carousel.js'));
+});
+
+app.get('/ui/js/wow.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'wow.min.js'));
+});
 
 function hash (input, salt) {
     // How do we create a hash?
