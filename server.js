10beead858835f72803aea25e186d56997e95c13
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-11 13:21:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -297,6 +297,10 @@ app.get('/ui/copy.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'copy.jpg'));
 });
 
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
