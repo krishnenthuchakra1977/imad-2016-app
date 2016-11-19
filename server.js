580b43ebfb6a9200deaf992c2d2c263a2fe1280a
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-19 09:59:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -68,18 +68,13 @@ function createTemplate (data) {
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
 
 function hash (input, salt) {
     // How do we create a hash?
