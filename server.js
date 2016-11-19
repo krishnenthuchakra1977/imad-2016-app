72e96d7975b07cd6acb6be25c9ec040987765e22
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-19 09:11:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 function hash (input, salt) {
     // How do we create a hash?
