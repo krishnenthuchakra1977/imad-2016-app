2a50aba0adbc0f558fae53cd71b8b64368a6b1a9
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-24 01:04:26 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
 
 function loadLoginForm () {
     var loginHtml = `
-       <form class="form-signin">
-			<h3 class="form-signin-heading">Login/Register!</h3>
-			<label>Username</label>
-			<input type="text" class="form-control" id="username" placeholder="Username" required autofocus>
-			<label>Password</label>
-			<input type="password" class="form-control" id="password" placeholder="Password" required>
-			<button id="login_btn" class="btn btn-primary btn-signin" type="submit">Login</button>
-			<button id="register_btn" class="btn btn-primary btn-signin" type="submit">Register</button>
-		</form>
+       <h3>Login/Register Here for Posting Comments on the Articles!</h3>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter Your Username" required>
+            </div>
+        </div>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" required>
+            </div>
+        </div>
+        <br/>
+        <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
+        <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
