e0d43b90b81332f3ed3dedbaaccea1719c1b6cc6
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-24 01:07:23 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,12 @@
 
 function loadLoginForm () {
     var loginHtml = `
-       <h3>Login/Register Here for Posting Comments on the Articles!</h3>
-        <div class="row control-group">
-            <div class="form-group col-xs-12 floating-label-form-group controls">
-              <label>Username</label>
-                <input type="text" class="form-control" id="username" placeholder="Enter Your Username" required>
-            </div>
-        </div>
-        <div class="row control-group">
-            <div class="form-group col-xs-12 floating-label-form-group controls">
-              <label>Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Password" required>
-            </div>
-        </div>
-        <br/>
-        <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
-        <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
