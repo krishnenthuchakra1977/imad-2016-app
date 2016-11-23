a4e2f44dca9c8e12e15f5b3b65cb2ab296383057
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-24 00:24:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -282,12 +282,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-//counter code
-var counter = 0;
-app.get('/counter',function(req,res){
-    counter = counter+1;
-   res.send(counter.toString()); 
-});
+
 
 function hash (input, salt) {
     // How do we create a hash?
