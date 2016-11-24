7bf6b54a7136c777e1d3d220653a6c0794fbb2c8
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-24 22:09:23 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -152,33 +152,8 @@ request.onreadystatechange = function() {
         }
     }
 };
-         
-         //site visit blog
-         function getCounter(){
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-};
-                  //site visit article
-                  function getCounter(){
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-};
-                           
-
+        
+                                         
  request.open('GET', '/counter', true);
     request.send(null);
 }
