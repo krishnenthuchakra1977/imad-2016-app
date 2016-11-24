7357ee69eb275c7551d86e1fe81ff7ec7ae8816b
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-24 19:04:13 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -127,18 +127,7 @@ function loadLogin () {
 }
 
 
-//site visit counter
-function getCounter(){
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-};
+
  request.open('GET', '/counter', true);
     request.send(null);
 }
