4487b6af50b3a1f797e0c4b6570e04f8ced6be18
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-24 22:11:13 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -147,7 +147,7 @@ request.onreadystatechange = function() {
     if(request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
             var countered = request.responseText;
-            var spanner = document.getElementById('count');
+            var spanner = document.getElementById('countw');
             span.innerHTML = counter.toString();
         }
     }
