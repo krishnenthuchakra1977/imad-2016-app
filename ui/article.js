6c089c534386b8d15c14dc845171ae0dbf7d253a
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-25 20:40:55 +0530

    Update article.js

--- ui/article.js
+++ ui/article.js
@@ -10,7 +10,7 @@ function loadCommentForm () {
         </div>
         </div>
         <br/>
-        <input type="submit" class="btn btn-default" id="submit" value="Submit" />
+        <input type="submit" class="btn btn-danger form-button" id="submit" value="Submit" />
         <br/>
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
