bc3a99fe5271a06f1605ad23815ebf8722316798
n.c.krishnenthu chakra <krishnenthuchakra1977@gmail.com>
2016-11-29 18:47:41 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -177,30 +177,7 @@ function loadArticles () {
         }
     };
          
-        ` $( document ).ready(function() {
-	// Handler for .ready() called.
-
-	$(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-	});
-
-	// scroll body to 0px on click
-	$('#back-to-top').click(function () {
-		$('#back-to-top').tooltip('hide');
-		$('body,html').animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
         
-    $('#back-to-top').tooltip('show');
-                  
-         });
-	`
     
     request.open('GET', '/get-articles', true);
     request.send(null);
